e0258bacfefafd78aab7afdbd2023a6efd8c8873
Franco Abosso <franco.abosso@gm2dev.com>
2021-11-23 18:11:29 -0300

    first commit for lambda 2

---
+++ lambda_2/index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+  console.log("Changed pushed to lambda 1 from repo!");
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
+};
